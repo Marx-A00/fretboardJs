409aa1fbc25bfeb7978f6ef00bf901daf405738e
Marcos Andrade <mnandrade1999@gmail.com>
2025-03-06 17:17:52 -0600

    generating note names based on index

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const numberOfStrings = 6;
 const singleFretMarkPositions = [3,5,7,9,15,17,19,21];
 const doubleFretMarkPositions = [12,24];
 
+const notesFlat = ["C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"]
+const notesSharp = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+
+let accidentals = 'flats'
+
 const app = {
     init(){
         this.setupFretboard();
@@ -40,6 +45,19 @@ const app = {
                 }
             }
         }
+    },
+    generateNoteNames(noteIndex,accidentals){
+        noteIndex = noteIndex % 12;
+        let noteName;
+        if (accidentals === 'flats'){
+            noteName = notesFlat[noteIndex]
+        }
+        else if (accidentals === 'sharps'){
+            noteName = notesSharp[noteIndex]
+        }
+
+        return noteName;
+
     }
 }
 
